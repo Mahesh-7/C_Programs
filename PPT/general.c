c9be73f6345aab475a969552c6d978bb6f12d7a8
MAHESH-K <60285145+Mahesh-7@users.noreply.github.com>
2020-11-10 18:24:07 +0530

    Add files via upload

---
+++ PPT/general.c
@@ -0,0 +1,76 @@
+#include <stdio.h>
+
+#include <stdlib.h>
+
+/*
+//jumping statement /non conditional / case statement
+int main()
+{
+	    hi:
+    	printf("hi");
+    int i=0;
+    for(;i<10;i++)  //counter control loop
+    {
+    		i=i/10;  //sentinel control loop
+    	
+            if(i==5) 
+            {
+                goto hi;
+            }
+            
+        printf("%d ",i);
+    }
+
+}
+
+//break o/p    0,1,2,3,4
+//continue o/p    0,1,2,3,4,6,7,8,9
+//return o/p	0,1,2,3,4
+//goto o/p	0,1,2,3,4 hi 
+
+//break vs return
+
+//break ->exit from loop only 
+//return exit from function
+*/
+/*
+int main()
+{
+    int i=5;
+    if (printf("\0"))  //return value from pf is zero
+        i = 3;
+    else
+        i = 5;
+    printf("%d", i);
+    return 0;
+}
+
+*/
+
+
+/*    ////nested printf 
+
+int main()
+{
+    	int i =1;
+      printf("%d", printf("%d .%d",1,1));  //nested printf   1 .14
+     return 0;
+}
+
+*/
+
+
+
+/*   //little vs big 
+
+
+int main()
+{
+        int a=0x04030201;
+        
+        printf("%d",a&0xff);
+}
+
+*/
+
+
